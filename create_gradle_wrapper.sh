4756aff1f134e118ae10b48a3bf4c2ec974d102e
Arthur Picht <picht@arthurpicht.de>
2021-02-03 13:47:42 +0100

    install gradle wrapper manually

--- create_gradle_wrapper.sh
+++ create_gradle_wrapper.sh
@@ -1,5 +1,18 @@
 
-gradle wrapper
-./gradlew wrapper --gradle-version 6.7.1
+# gradle wrapper
+# ./gradlew wrapper --gradle-version 6.7.1
+# ./gradlew --version
+
+mkdir -p gradle/wrapper
+cd gradle/wrapper
+
+wget https://raw.githubusercontent.com/gradle/gradle/master/gradle/wrapper/gradle-wrapper.jar
+wget https://raw.githubusercontent.com/gradle/gradle/master/gradle/wrapper/gradle-wrapper.properties
+
+cd ../..
+java -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain init
+java -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain wrapper --gradle-version 6.7.1
 ./gradlew --version
+
+
 
